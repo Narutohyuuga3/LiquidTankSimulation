49f13f13a5c932776bfcc513d02b10794803fbbc
Narutohyuuga3 <michael.unseld@online.de>
2022-10-09 01:10:49 +0900

    Kalman filter implemented
    test setup with plots

--- Source/main.py
+++ Source/main.py
@@ -1,31 +1,45 @@
 import sys
 
-from Model.gamefield import Gamefield
-from View import resources
+from model.gamefield import gamefield
+from view import resources
 
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtCore import QTimer
 
-def on_keyLeft():
-    print("Key left pressed!")
+class MainWindow(QQmlApplicationEngine):
+    def __init__(self, gamefield):
+        self._field = gamefield
+        QQmlApplicationEngine.__init__(self)
+        self.quit.connect(app.quit)
+        self.load('Source/View/main.qml')
+
+    def exit(self, event):
+        print("Mainwindow exit: called")
+        self._field.__del__()
+        
 
 if __name__ == '__main__':
     app = QGuiApplication(sys.argv)
 
-    engine = QQmlApplicationEngine()
-    engine.quit.connect(app.quit)
-    engine.load('Source/View/main.qml')
-
     spaceshipPosX = 200
     spaceshipPosY = 200
-    gamefield = Gamefield()
-    engine.rootObjects()[0].setProperty('gamefield', gamefield)
+    field = gamefield()
+    engine = MainWindow(field)
+
+    engine.rootObjects()[0].setProperty('gamefield', field)
     root = engine.rootObjects()[0]
-    root.startKeyPressed.connect(gamefield.on_keyPressed)
-    root.stopKeyPressed.connect(gamefield.on_keyReleased)
-    gamefield.updateSpaceshipPos.connect(root.onUpdateSpaceshipPos)
-    gamefield.updateSpaceshipEstimation.connect(root.onUpdateSpaceshipEstimation)
-    gamefield.updateSpaceshipMeasurepoint.connect(root.onUpdateSpaceshipMeasurepoint)
+    root.startKeyPressed.connect(field.on_keyPressed)
+    root.stopKeyPressed.connect(field.on_keyReleased)
+    field.updateSpaceshipPos.connect(root.onUpdateSpaceshipPos)
+    field.updateSpaceshipEstimation.connect(root.onUpdateSpaceshipEstimation)
+    field.updateSpaceshipMeasurepoint.connect(root.onUpdateSpaceshipMeasurepoint)
+    app.lastWindowClosed.connect(field.__del__)
+
+    timer = QTimer()
+    timer.timeout.connect(lambda: None)
+    timer.start(100)
+
 
     if not engine.rootObjects():
         sys.exit(-1)
